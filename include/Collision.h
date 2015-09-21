f54089eb15ac27c9841500c936b0ed01592416c8
Charlesfire <charlesfire2@hotmail.com>
2015-09-20 22:01:25 -0400

    Ajout du namespace collision
    Début d'implémentation du PhysicWorld.

---
+++ include/Collision.h
@@ -0,0 +1,9 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+namespace Collision
+{
+
+}
+
+#endif // COLLISION_H
--- include/PhysicWorld.h
+++ include/PhysicWorld.h
@@ -1,13 +1,18 @@
 #ifndef PHYSICWORLD_H
 #define PHYSICWORLD_H
 
+#include <vector>
+
 class PhysicWorld
 {
     public:
         PhysicWorld();
         virtual ~PhysicWorld();
+        void Update();
+        void AddBody(PhysicBody* body);
     private:
         float friction;
+        std::vector<PhysicBody*> bodies;
 };
 
 #endif // PHYSICWORLD_H
